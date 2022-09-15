fffba3ce91b9eb5b5cfc5803ee8f2211aaf49b02
MateuszKociolek <mkociolek2005@gmail.com>
2022-09-15 23:28:36 +0200

    added a source

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="App">
+    <h1>Hello Vue!</h1>
+    <component1></component1>
+    <comp2></comp2>
+  </div>
+</template>
+
+<script>
+import component1 from "./components/component1.vue"
+import comp2 from "./components/comp2.vue"
+
+
+export default{
+  components: {
+    component1: component1,
+    comp2: comp2,
+  },
+  data() {
+    return {
+      count: 1,
+    }
+  },
+}
+
+
+</script>
+
+<style>
+  
+</style>
---
+++ src/components/component1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="all">
+        <h1>Hello from component1</h1>
+        <p>{{counter1}}</p>
+        <button @click="counter1++">App</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            counter1: 1
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .all{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: red;
+        width:50vw;
+        height: 300px;
+    }
+</style>
